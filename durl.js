67f664779e5682af72a0d0bc9ce84ad8fe9d3eaf
Shane Husson <s.husson@garvan.org.au>
2017-04-02 14:35:25 +1000

    use webtask programming model

--- durl.js
+++ durl.js
@@ -1,12 +1,15 @@
-const AWS = require('aws-sdk');
+module.exports = function(context, cb) {
+  const AWS = require('aws-sdk@2.5.3')
+  const s3 = new AWS.S3();
+  AWS.config.update({accessKeyId: context.awsId, secretAccessKey: context.awsSecret})
+  const b = 'sgc-download-test';
+  const k = 'test.vcf';
+  const e = 1;
 
-const s3 = new AWS.S3();
-const b = 'sgc-download-test';
-const k = 'test.vcf';
-const e = 1;
-
-const url = s3.getSignedUrl('getObject', {
-    Bucket: b,
-    Key: k,
-    Expires: e
-});
+  const url = s3.getSignedUrl('getObject', {
+      Bucket: b,
+      Key: k,
+      Expires: e
+  });
+  cb(null, url);
+}
