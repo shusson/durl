e5635b44699c05f48e4ce5dc4fef4a9aa961708d
Shane Husson <s.husson@garvan.org.au>
2017-04-02 15:53:11 +1000

    construct s3 service with credentials - for some reason the global credentials were causing timeout erros

--- durl.js
+++ durl.js
@@ -1,15 +1,15 @@
 module.exports = function(context, cb) {
-  const AWS = require('aws-sdk@2.5.3')
-  const s3 = new AWS.S3();
-  AWS.config.update({accessKeyId: context.awsId, secretAccessKey: context.awsSecret})
+  const AWS = require('aws-sdk')
   const b = 'sgc-download-test';
   const k = 'test.vcf';
-  const e = 1;
-
+  const e = 60;
+  const creds = new AWS.Credentials(context.secrets.awsId, context.secrets.awsSecret);
+  const s3 = new AWS.S3({credentials: creds});
   const url = s3.getSignedUrl('getObject', {
       Bucket: b,
       Key: k,
       Expires: e
+  }, function (err, url) {
+    cb(err, url);
   });
-  cb(null, url);
 }
